c1539f6442df8bec22d5cd1109da7f92926133a0
Prokhor Minin <prokhor@len0xx.space>
2022-05-21 17:41:29 +0500

    LandmarkList ana LandmarkRow views

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Prokhor Minin on 21.05.2022.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
