e3dceb861f3eac2ab9ec0807eb38e05684f28885
Prokhor Minin <prokhor@len0xx.space>
2022-05-21 14:28:21 +0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Prokhor Minin on 21.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
